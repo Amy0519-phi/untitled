3f75dc9af86d530c066027ad9ca9291c39da3546
bixiang666 <13072864+bixiang666@user.noreply.gitee.com>
2023-08-25 15:47:32 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.servlet.http.HttpServletRequest;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        HttpServletRequest request=(HttpServletRequest) serveletRequest;
+
+
+    }
+}
